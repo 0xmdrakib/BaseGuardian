365815a6db6d6f1c843e1bd0c1947c7468f7dd64
Md. Rakib <113634586+0xmdrakib@users.noreply.github.com>
2025-12-11 21:21:50 +0600

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,14 +3,6 @@ export default function Head() {
   return (
     <>
       <title>Base Guardian</title>
-
-      {/* REQUIRED for Base Build ownership check */}
-      <meta
-        name="base:app_id"
-        content="693acb1de6be54f5ed71d631"
-      />
-
-      {/* Optional: Farcaster mini app preview */}
       <meta
         name="fc:miniapp"
         content={JSON.stringify({
